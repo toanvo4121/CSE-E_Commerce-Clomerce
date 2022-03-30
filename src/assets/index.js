defb9451f327f57cf0f388a66b41f57d73bc3070
Võ Minh Toàn <toanvo7120@gmail.com>
2022-03-30 22:21:54 +0700

    avatar

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,5 @@
-export { default as Mem1 } from './Shiba_1.jpg';
-export { default as Mem2 } from './Shiba_2.jpg';
-export { default as Mem3 } from './Shiba_3.jpeg';
-export { default as Mem4 } from './Husky_1.jpg';
-export { default as Mem5 } from './Husky_2.png';
+export { default as Mem1 } from './Mem1.jpg';
+export { default as Mem2 } from './Mem2.jpg';
+export { default as Mem3 } from './Mem3.jpg';
+export { default as Mem4 } from './Mem4.jpg';
+export { default as Mem5 } from './Mem5.jpg';
